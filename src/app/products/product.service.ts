83445c6ca8e528fddd696a8b056b12c5a0974f53
Franz Lycans <blicanel@yahoo.com>
2018-01-15 22:44:47 +0800

    added firestore, (Forgot to connect hahaha)

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -7,58 +7,16 @@ import { Product } from './product-model';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 
-interface NewProduct {
-  id?: string;
-  title: number;
-  description: string;
-  price: number;
-  dateCreated: number;
-}
+
 
 @Injectable()
 export class ProductService {
-
-  notesCollection: AngularFirestoreCollection<Product>;
-  noteDocument:   AngularFirestoreDocument<Product>;
+  
+  productCollection: AngularFirestoreCollection<Product>;
+  productDocument:   AngularFirestoreDocument<Product>;
 
   constructor(private afs: AngularFirestore) {
-    this.productsCollection = this.afs.collection('products', (ref) => ref.orderBy('dateCreated', 'desc').limit(5));
+    this.notesCollection = this.afs.collection('notes', (ref) => ref.orderBy('time', 'desc').limit(5));
   }
-
-  getData(): Observable<Product[]> {
-    return this.productsCollection.valueChanges();
-  }
-
-  getSnapshot(): Observable<Product[]> {
-    // ['added', 'modified', 'removed']
-    return this.productsCollection.snapshotChanges().map((actions) => {
-      return actions.map((a) => {
-        const data = a.payload.doc.data() as Product;
-        return { id: a.payload.doc.id, content: data.content, hearts: data.hearts, dateCreated: data.time };
-      });
-    });
-  }
-
-  getProduct(id: string) {
-    return this.afs.doc<Product>(`products/${id}`);
-  }
-
-  create(content: string) {
-    const product = {
-      id,
-      title,
-      description,
-      price: 0,
-      dateCreated: new Date().getTime(),
-    };
-    return this.productsCollection.add(product);
-  }
-
-  updateProduct(id: string, data: Partial<Product>) {
-    return this.getProduct(id).update(data);
-  }
-
-  deleteProduct(id: string) {
-    return this.getProduct(id).delete();
-  }
+  
 }
